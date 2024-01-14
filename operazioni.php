f2e250aae90912ae125606576684ed87d859adff
pceres <pasquale_c@hotmail.com>
2024-01-14 20:00:00 +0100

    add examples to list of operations
    modificato:             operazioni.php

--- operazioni.php
+++ operazioni.php
@@ -7,7 +7,12 @@
 
 <h2>Operazioni passo-passo</h2>
 
+I calcoli vengono effettuati passo-passo, come se fossero svolti manualmente su carta.<br>
+Lo scopo è aiutare i bambini (e i genitori che controllano i loro compiti) nelle operazioni matematiche.<br>
 
+I calcoli passo-passo vengono svolti su interi, ma vengono gestiti anche numeri decimali.<br>
+<br>
+I calcoli disponibili sono:<br>
 <ul>
     <li>
         <a href="prodotto.php">prodotto</a>
@@ -16,7 +21,74 @@
     </li>
 </ul>
 
+
+Esempio prodotto 232*98227:<br>
+<br>
+<!-- show table -->
+<b>
+<table>
+<tr>
+	<td>&ensp;</td><td>&ensp;</td><td>&ensp;</td><td>&ensp;</td><td>&ensp;</td><td>2</td><td>3</td><td>2</td><td>&ensp;</td><td>x</td><td></td>
+</tr><tr>
+	<td>&ensp;</td><td>&ensp;</td><td>&ensp;</td><td>9</td><td>8</td><td>2</td><td>2</td><td>7</td><td>&ensp;</td><td>=</td><td></td>
+</tr><tr>
+	<td>-</td><td>-</td><td>-</td><td>-</td><td>-</td><td>-</td><td>-</td><td>-</td><td>-</td><td>-</td><td></td>
+</tr><tr>
+	<td>&ensp;</td><td>&ensp;</td><td>&ensp;</td><td>&ensp;</td><td>1</td><td>6</td><td>2</td><td>4</td><td>&ensp;</td><td></td><td></td>
+</tr><tr>
+	<td>&ensp;</td><td>&ensp;</td><td>&ensp;</td><td>&ensp;</td><td>4</td><td>6</td><td>4</td><td>0</td><td>&ensp;</td><td>&ensp;</td><td></td>
+</tr><tr>
+	<td>&ensp;</td><td>&ensp;</td><td>&ensp;</td><td>4</td><td>6</td><td>4</td><td>0</td><td>0</td><td>&ensp;</td><td>&ensp;</td><td></td>
+</tr><tr>
+	<td>&ensp;</td><td>1</td><td>8</td><td>5</td><td>6</td><td>0</td><td>0</td><td>0</td><td>&ensp;</td><td>&ensp;</td><td></td>
+</tr><tr>
+	<td>2</td><td>0</td><td>8</td><td>8</td><td>0</td><td>0</td><td>0</td><td>0</td><td>&ensp;</td><td>&ensp;</td><td></td>
+</tr><tr>
+	<td>-</td><td>-</td><td>-</td><td>-</td><td>-</td><td>-</td><td>-</td><td>-</td><td>-</td><td>-</td><td></td>
+</tr><tr>
+	<td>2</td><td>2</td><td>7</td><td>8</td><td>8</td><td>6</td><td>6</td><td>4</td><td>&ensp;</td><td>&ensp;</td><td></td>
+</tr><tr>
+	<td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td>
+</tr>
+</table>
+</b>
+
+</td></tr></table>
+<br>
+
+
+
+Esempio divisione 1345*21:<br>
+<br>
+<!-- show table -->
+<b>
+<table>
+<tr>
+	<td>/</td><td>-</td><td>\</td><td>^</td><td>&ensp;</td><td>&ensp;</td><td>&ensp;</td><td>&ensp;</td><td>&ensp;</td><td></td>
+</tr><tr>
+	<td>1</td><td>3</td><td>4</td><td>5</td><td>&ensp;</td><td>:</td><td>&ensp;</td><td>2</td><td>1</td><td></td>
+</tr><tr>
+	<td>&ensp;</td><td>&ensp;</td><td>&ensp;</td><td>&ensp;</td><td>&ensp;</td><td>-</td><td>-</td><td>-</td><td>-</td><td>-</td>
+</tr><tr>
+	<td>1</td><td>3</td><td>4</td><td>&ensp;</td><td>&ensp;</td><td>|</td><td>&ensp;</td><td>6</td><td>4</td><td></td>
+</tr><tr>
+	<td>1</td><td>2</td><td>6</td><td>&ensp;</td><td>&ensp;</td><td>|</td><td>&ensp;</td><td>&ensp;</td><td>&ensp;</td><td></td>
+</tr><tr>
+	<td>0</td><td>0</td><td>8</td><td>5</td><td>&ensp;</td><td>|</td><td>&ensp;</td><td>&ensp;</td><td>&ensp;</td><td></td>
+</tr><tr>
+	<td>&ensp;</td><td>&ensp;</td><td>8</td><td>4</td><td>&ensp;</td><td>|</td><td>&ensp;</td><td>&ensp;</td><td>&ensp;</td><td></td>
+</tr><tr>
+	<td>&ensp;</td><td>&ensp;</td><td>&ensp;</td><td>1</td><td>&ensp;</td><td>|</td><td>&ensp;</td><td>&ensp;</td><td>&ensp;</td><td></td>
+</tr><tr>
+	<td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td>
+</tr>
+</table>
+</b>
+
+</td></tr></table>
+<br>
+
+
+Il codice sorgente in linguaggio PHP è liberamente disponibile <a href="https://github.com/pceres/math_for_kid">qui</a>
 </body>
 </html>
-
-
