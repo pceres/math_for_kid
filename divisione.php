6c0fb03d8d69b1b3a240aebe22bb630c0f3d3004
pceres <pasquale_c@hotmail.com>
2024-01-15 22:27:39 +0100

    divisione: configure possibility to show product
    modificato:             divisione.php
    	some prefer showing a roew dedicated to
    	the product of the last figure by the
    	factor, some others do the calc in their
     	mind

--- divisione.php
+++ divisione.php
@@ -7,12 +7,20 @@
 
 <?php
 
-require_once ('libreria.php'); // load library
+require_once ('libreria.php'); // carica libreria
 
 
-if (isset($_POST['SubmitButton'])) {    //check if form was submitted
-  $input1 = $_POST['fDividendo'];   //get input text
-  $input2 = $_POST['lDivisore'];    //get input text
+if (isset($_POST['SubmitButton'])) {  //verifica se si arriva dopo aver cliccato il tasto "Calcola"
+    $input1 = $_POST['fDividendo'];   //get input text
+    $input2 = $_POST['lDivisore'];    //get input text
+    $input3 = $_POST['fMostraProd'];  //get input text
+}
+else
+{
+    // valori di default
+    $input1 = 1345;
+    $input2 = 2.1;
+    $input3 = "on";
 }
 
 ?>
@@ -25,7 +33,10 @@ Inserisci i fattori per il calcolo della divisione, e clicca il tasto "Calcola"
     <label for="fDividendo">Dividendo:</label><br>
     <input type="text" id="fDividendo" name="fDividendo" value="<?=$input1?>"><br>
     <label for="lDivisore">Divisore:</label><br>
-    <input type="text" id="lDivisore" name="lDivisore" value="<?=$input2?>">
+    <input type="text" id="lDivisore" name="lDivisore" value="<?=$input2?>"><br>
+    <br>
+    <input type="checkbox" id="fMostraProd" name="fMostraProd" <?=is_null($input3)?"":"checked";?>>
+    <label for="fMostraProd">Mostra la riga del prodotto</label><br>
 
     <input type="submit" value="Calcola" name="SubmitButton">
 </form>
@@ -40,17 +51,19 @@ Inserisci i fattori per il calcolo della divisione, e clicca il tasto "Calcola"
 if (isset($_POST['SubmitButton'])) { //check if form was submitted
   $input1 = $_POST['fDividendo'];   //get input text
   $input2 = $_POST['lDivisore'];    //get input text
+  $input3 = $_POST['fMostraProd'];  //get input text
   $n1 = floatval($input1);
   $n2 = floatval($input2);
-  // echo("Input are: $input1 : $input2");
-  divisione($n1,$n2);
+  $flg_show_prodotto = ($input3 == "on");
+  // echo("Input are: |$input1:$input2-$input3|");
+  divisione($n1,$n2,$flg_show_prodotto);
 } else {
     divisione();
 }
 
 
 
-function divisione($n1 = 1345, $n2 = 2.1) {
+function divisione($n1 = 1345, $n2 = 2.1, $flg_show_prodotto = true) {
 
 $result = forma_canonica_divisione($n1,$n2);
 $k1       = $result['k1'];
@@ -63,105 +76,124 @@ $debug = 0;
 
 echo_my("n1:$n1 - n2:$n2 - k1: $k1 - k2:$k2 - num_dec1:$num_dec1 - num_dec2:$num_dec2 - num_1st:$num_1st<br>",$debug);
 
-// var_dump($result);
-$page = Array();
+if ($n2 == 0) {
+    echo("Attenzione! Il divisore non può essere 0!<br>");
+}
+else
+{
+    // esegui la divisione
 
-// $page = scrivi($page,1,1,"*");
-// $page = scrivi($page,-3,1,"*234567");
-// $page = scrivi($page,1,-50," ");
+    // var_dump($result);
+    $page = Array();
 
-$page = scrivi($page,-1,1-strlen($k1),"$k1 :");
+    // $page = scrivi($page,1,1,"*");
+    // $page = scrivi($page,-3,1,"*234567");
+    // $page = scrivi($page,1,-50," ");
 
-// $page = scrivi($page,-1,4,"$k2 =");
-$page = scrivi($page,-1,4,$k2);
+    $page = scrivi($page,-1,1-strlen($k1),"$k1 :");
 
-$num_cifre_out = strlen($k1)-$num_1st+1; // numero cifre quoziente
-// $page = scrivi($page,0,-strlen($k1)+1,str_repeat('-',strlen($k1)+$num_cifre_out+3));
-$page = scrivi($page,0,2,str_repeat('-',$num_cifre_out+3));
+    // $page = scrivi($page,-1,4,"$k2 ="); // mostra anche l'uguale
+    $page = scrivi($page,-1,4,$k2);
 
-$num_abbasso = $num_1st; // numero cifre da abbassare
-$pos_abbasso = 0; // posizione cifre da abbassare in $k1 (0-based)
-$accum = ""; // accumulatore cifre abbassate
-$ks_resto = "";
-$quoz = ""; // quoziente
+    $num_cifre_out = strlen($k1)-$num_1st+1; // numero cifre quoziente
+    // $page = scrivi($page,0,-strlen($k1)+1,str_repeat('-',strlen($k1)+$num_cifre_out+3)); // la linea passa anche sotto al dividendo
+    $page = scrivi($page,0,2,str_repeat('-',$num_cifre_out+3)); // linea solo sotto il divisore
 
-// $sub = Array();
-for ($i_level=1;$i_level<=$num_cifre_out;$i_level++) {
 
-    $accum = $ks_resto.substr($k1,$pos_abbasso,$num_abbasso); // prime cifre abbassate
-    $riga_base = ($i_level-1)*2+1;
-
-    $col_abbasso = -strlen($k1)+$pos_abbasso+$num_abbasso-1; // colonna ultima cifra "abbassata" (in $page)
-
-    echo_my("iter $i_level/$num_cifre_out: accum:$accum - pos_abbasso:$pos_abbasso - num_abbasso:$num_abbasso - col_abbasso:$col_abbasso - riga_base: $riga_base<br>",$debug);
-
-    $page = abbassa($page,-2,$col_abbasso-$num_abbasso+2,$pos_abbasso,$num_abbasso); // scrivi archetto in alto per indicare cifre abbassate
-    $page = scrivi($page,$riga_base,$col_abbasso-strlen($accum)+2,$accum); // scrivi accumulatore (resto + ultima cifra abbassata)
-    echo_my("--1> $riga_base,$col_abbasso-strlen($ks_resto),$accum<br>",$debug);
-
-    // barra verticale
-    $cifra = floor(intval($accum) / intval($k2));
-    $page = scrivi($page,$riga_base,2,"|");
-    $page = scrivi($page,$riga_base+1,2,"|");
-    $page = scrivi($page,$riga_base+2,2,"|");
-
-    // divisione
-    $page = scrivi($page,1,3+$i_level,strval($cifra),$debug);
-    $quoz .= $cifra;
-    echo_my("$i_level) scrivo $cifra in riga 1, colonna 3+$i_level<br>",$debug);
-
-    // prodotto
-    $prod = $cifra*intval($k2);
-    $page = scrivi($page,$riga_base+1,$col_abbasso+2-strlen($prod),strval($prod));
-
-    // resto
-    $resto = intval($accum) % intval($k2);
-    $ks_resto = strval($resto);
-    $ks_resto = str_repeat("0",max(0,$num_abbasso-strlen($ks_resto))).$ks_resto;
-    $page = scrivi($page,$riga_base+2,$col_abbasso+2-strlen($ks_resto),$ks_resto);
-
-    $accum = $ks_resto;
-    $pos_abbasso += $num_abbasso; // aggiorna posizione cifre da abbassare
-    $num_abbasso = 1; // d'ora in avanti, una cifra alla volta
-    $riga_base += 2;
-
-    echo_my("cifra:$cifra - resto:$resto<br>",$debug);
-    if ($debug) {
-        stampa($page);
+    if ($flg_show_prodotto) {
+        $off_prodotto = 1; // il resto andrà una riga sotto, visto che si visualizza anche il prodotto
     }
-}
+    else
+    {
+        $off_prodotto = 0; // non visualizzando il resto, non serve scendere di una riga sotto
+    }
 
-echo("<table>\n\t<tr>\n\t\t<td style=\"width:30%\"></td><td>\n\n");
-stampa($page);
-echo("\n</td></tr></table>\n\n");
+    $num_abbasso = $num_1st; // numero cifre da abbassare
+    $pos_abbasso = 0; // posizione cifre da abbassare in $k1 (0-based)
+    $accum = ""; // accumulatore cifre abbassate
+    $ks_resto = "";
+    $quoz = ""; // quoziente
 
-// echo('<br>');
+    // per ogni volta che si abbassa, itera:
+    for ($i_level=1;$i_level<=$num_cifre_out;$i_level++) {
 
-$num_dec_out = max(0,$num_dec1-$num_dec2);
-$num_dec_resto = max(0,$num_dec1);
+        $accum = $ks_resto.substr($k1,$pos_abbasso,$num_abbasso); // prime cifre abbassate
+        $riga_base = ($i_level-1)*(1+$off_prodotto)+1;
 
-$fmt1    = sprintf("%%.%df",$num_dec1);
-$fmt2    = sprintf("%%.%df",$num_dec2);
-$fmt_out = sprintf("%%.%df",$num_dec_out);
-$fmt_resto = sprintf("%%.%df",$num_dec_resto);
+        $col_abbasso = -strlen($k1)+$pos_abbasso+$num_abbasso-1; // colonna ultima cifra "abbassata" (in $page)
 
-$ks1    = sprintf($fmt1,$n1);
-$ks2    = sprintf($fmt2,$n2);
-echo_my("fmt_out:$fmt_out - k1: $k1 - k2:$k2 - ks1: $ks1 - ks2:$ks2 - num_dec1:$num_dec1 - num_dec2:$num_dec2 - num_1st:$num_1st - num_dec_resto:$num_dec_resto<br>",$debug);
-$ks_out__ = sprintf($fmt_out,intval($quoz)/pow(10,$num_dec1-$num_dec2));
-$ks_resto__ = sprintf($fmt_resto,intval($resto)/pow(10,$num_dec_resto));
+        echo_my("iter $i_level/$num_cifre_out: accum:$accum - pos_abbasso:$pos_abbasso - num_abbasso:$num_abbasso - col_abbasso:$col_abbasso - riga_base: $riga_base<br>",$debug);
 
-echo("<br>\n");
-if ( ($num_dec1>0) || ($num_dec2>0) ) {
-    echo("<!-- info on decimal digits -->\n");
-    echo("Il dividendo $ks1 ha ".strval($num_dec1)." ".text_cifre_decimali($num_dec1)."<br>\n");
-    echo("Il divisore $ks2 ha ".strval($num_dec2)." ".text_cifre_decimali($num_dec2)."<br>\n");
-    echo("Il risultato avrà ".strval($num_dec1-$num_dec2)." ".text_cifre_decimali($num_dec1-$num_dec2)."<br>\n");
+        $page = abbassa($page,-2,$col_abbasso-$num_abbasso+2,$pos_abbasso,$num_abbasso); // scrivi archetto in alto per indicare cifre abbassate
+        $page = scrivi($page,$riga_base,$col_abbasso-strlen($accum)+2,$accum); // scrivi accumulatore (resto + ultima cifra abbassata)
+        echo_my("--1> $riga_base,$col_abbasso-strlen($ks_resto),$accum<br>",$debug);
+
+        // barra verticale
+        $cifra = floor(intval($accum) / intval($k2));
+        $page = scrivi($page,$riga_base,2,"|");
+        $page = scrivi($page,$riga_base+1,2,"|");
+
+        // nuova cifra della divisione
+        $page = scrivi($page,1,3+$i_level,strval($cifra),$debug);
+        $quoz .= $cifra;
+        echo_my("$i_level) scrivo $cifra in riga 1, colonna 3+$i_level<br>",$debug);
+
+        if ($flg_show_prodotto) {
+            // mostra riga del prodotto, se richiesto
+            $prod = $cifra*intval($k2);
+            $page = scrivi($page,$riga_base+1,$col_abbasso+2-strlen($prod),strval($prod));
+            $page = scrivi($page,$riga_base+2,2,"|"); // aggiungi anche una barretta verticale in più
+        }
+
+        // resto
+        $resto = intval($accum) % intval($k2);
+        $ks_resto = strval($resto);
+        $ks_resto = str_repeat("0",max(0,$num_abbasso-strlen($ks_resto))).$ks_resto;
+        $page = scrivi($page,$riga_base+(1+$off_prodotto),$col_abbasso+2-strlen($ks_resto),$ks_resto);
+
+        $accum = $ks_resto;
+        $pos_abbasso += $num_abbasso; // aggiorna posizione cifre da abbassare
+        $num_abbasso = 1; // d'ora in avanti, una cifra alla volta
+        $riga_base += 1+$off_prodotto;
+
+        echo_my("cifra:$cifra - resto:$resto<br>",$debug);
+        if ($debug) {
+            stampa($page);
+        }
+    }
+
+    echo("<table>\n\t<tr>\n\t\t<td style=\"width:30%\"></td><td>\n\n");
+    stampa($page);
+    echo("\n</td></tr></table>\n\n");
+
+    // echo('<br>');
+
+    $num_dec_out = max(0,$num_dec1-$num_dec2);
+    $num_dec_resto = max(0,$num_dec1);
+
+    $fmt1    = sprintf("%%.%df",$num_dec1);
+    $fmt2    = sprintf("%%.%df",$num_dec2);
+    $fmt_out = sprintf("%%.%df",$num_dec_out);
+    $fmt_resto = sprintf("%%.%df",$num_dec_resto);
+
+    $ks1    = sprintf($fmt1,$n1);
+    $ks2    = sprintf($fmt2,$n2);
+    echo_my("fmt_out:$fmt_out - k1: $k1 - k2:$k2 - ks1: $ks1 - ks2:$ks2 - num_dec1:$num_dec1 - num_dec2:$num_dec2 - num_1st:$num_1st - num_dec_resto:$num_dec_resto<br>",$debug);
+    $ks_out__ = sprintf($fmt_out,intval($quoz)/pow(10,$num_dec1-$num_dec2));
+    $ks_resto__ = sprintf($fmt_resto,intval($resto)/pow(10,$num_dec_resto));
+
     echo("<br>\n");
-}
+    if ( ($num_dec1>0) || ($num_dec2>0) ) {
+        echo("<!-- info on decimal digits -->\n");
+        echo("Il dividendo $ks1 ha ".strval($num_dec1)." ".text_cifre_decimali($num_dec1)."<br>\n");
+        echo("Il divisore $ks2 ha ".strval($num_dec2)." ".text_cifre_decimali($num_dec2)."<br>\n");
+        echo("Il risultato avrà ".max(0,strval($num_dec1-$num_dec2))." ".text_cifre_decimali($num_dec1-$num_dec2)."<br>\n");
+        echo("<br>\n");
+    }
 
-echo("<!-- final result -->\n");
-printf("$ks1 : $ks2 = $ks_out__ (col resto di $ks_resto__)<br>\n");
+    echo("<!-- final result -->\n");
+    printf("$ks1 : $ks2 = $ks_out__ (col resto di $ks_resto__)<br>\n");
+}
 
 echo("<!-- go back -->\n");
 echo("<br><br>");
